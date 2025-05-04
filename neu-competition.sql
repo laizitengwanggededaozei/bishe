afee446d6fccbb33a2ae252a9f2247b404ef9254
迟宽鹏 <128274142+laizitengwanggededaozei@users.noreply.github.com>
2025-05-04 23:42:53 +0800

    2.0,添加比赛模块，修复一点bug，引入大量bug，团队管理，指导团队报404.作品评审，作品提交按钮无反应，赛题的答题按钮是英文，并且点击报错，比赛进行展示赛事页面，点击以后才会显示最新比赛，这里不用改，只用修改跳转赛事页面即可，然后，从赛事页面进入，学生、教师两个用户依然能看到修改赛事详情的按钮。

--- neu-competition.sql
+++ neu-competition.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80034
  File Encoding         : 65001
 
- Date: 05/03/2025 04:59:28
+ Date: 04/05/2025 23:42:41
 */
 
 SET NAMES utf8mb4;
@@ -36,15 +36,56 @@ CREATE TABLE `competition`  (
   `commend` int NOT NULL,
   PRIMARY KEY (`com_id`, `user_id`) USING BTREE,
   INDEX `com_id`(`com_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of competition
 -- ----------------------------
-INSERT INTO `competition` VALUES (1, '全国大学生程序设计竞赛', '中国大学生程序设计竞赛组委会', '国家级', '计算机科学与技术', 'http://www.acm.org.cn/', '本竞赛由中国大学生程序设计竞赛组委会主办，旨在促进大学生程序设计能力的提高，推动计算机科学与技术的发展。', '提高大学生程序设计能力', '无', '/image/01.png', '0', 1);
+INSERT INTO `competition` VALUES (1, '全国大学生程序设计竞赛', '中国大学生程序设计竞赛组委会', '国家级', '计算机科学与技术', 'http://www.acm.org.cn/', '本竞赛由中国大学生程序设计竞赛组委会主办，旨在促进大学生程序设计能力的提高，推动计算机科学与技术的发展。', '提高大学生程序设计能力', '无', '/image/01.png', 'B组织者', 1);
 INSERT INTO `competition` VALUES (2, '全国大学生数学建模竞赛', '中国大学生数学建模竞赛组委会', '国家级', '数学与应用数学', 'http://www.comap.com/', '本竞赛旨在培养学生综合运用数学知识解决实际问题的能力，提高数学建模水平。', '培养学生综合运用数学知识解决实际问题的能力', '无', '/image/02.png', '0', 1);
 INSERT INTO `competition` VALUES (3, '全国大学生英语演讲比赛', '中国大学生英语演讲比赛组委会', '国家级', '外语', 'http://www.efc.edu.cn/', '本竞赛旨在提高大学生英语口语表达能力，促进英语教学改革。', '提高大学生英语口语表达能力', '无', '/image/03.png', '0', 1);
-INSERT INTO `competition` VALUES (5, '1', '1', '院级', '1', '1', '1', '1', '1', '/image/屏幕截图 2024-10-10 061551.png', 'B组织者', 0);
+INSERT INTO `competition` VALUES (6, '1', '1', '院级', '1', '1', '1', '1', '1', '/image/OIP-C.jpg', 'B组织者', 0);
+
+-- ----------------------------
+-- Table structure for competition_type
+-- ----------------------------
+DROP TABLE IF EXISTS `competition_type`;
+CREATE TABLE `competition_type`  (
+  `type_id` int NOT NULL AUTO_INCREMENT,
+  `discipline` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `type_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  PRIMARY KEY (`type_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of competition_type
+-- ----------------------------
+INSERT INTO `competition_type` VALUES (1, '计算机科学与技术', 'PROGRAMMING', '程序设计类竞赛，通常需要在线编程环境');
+INSERT INTO `competition_type` VALUES (2, '数学与应用数学', 'MODELING', '数学建模类竞赛，通常需要提交论文与代码');
+INSERT INTO `competition_type` VALUES (3, '外语', 'SPEAKING', '演讲类竞赛，通常需要提交语音/视频或现场演讲');
+
+-- ----------------------------
+-- Table structure for evaluation
+-- ----------------------------
+DROP TABLE IF EXISTS `evaluation`;
+CREATE TABLE `evaluation`  (
+  `evaluation_id` int NOT NULL AUTO_INCREMENT,
+  `submission_id` int NOT NULL,
+  `user_id` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `score` decimal(5, 2) NOT NULL,
+  `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `eval_time` datetime NOT NULL,
+  PRIMARY KEY (`evaluation_id`) USING BTREE,
+  INDEX `submission_id`(`submission_id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`submission_id`) REFERENCES `submission` (`submission_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `evaluation_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of evaluation
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for matches
@@ -76,12 +117,12 @@ CREATE TABLE `matches`  (
 -- ----------------------------
 -- Records of matches
 -- ----------------------------
-INSERT INTO `matches` VALUES (1, '第一届 全国大学生程序设计竞赛', '中国大学生程序设计竞赛组委会', '李娜娜', '刘丽丽', '林玲玲', '2024-06-01 08:00:00', '2024-06-01 18:00:00', '2024-05-25 23:59:59', '比赛将按规则进行评比', '参赛队伍需按照规定完成题目', '一等奖：5000元，二等奖：3000元，三等奖：1000元', '/image/01.png', 1, 'B组织者', 0, 1);
+INSERT INTO `matches` VALUES (1, '第一届 全国大学生程序设计竞赛', '中国大学生程序设计竞赛组委会', '李娜娜', '刘丽丽', '林玲玲', '2024-06-01 08:00:00', '2024-06-01 18:00:00', '2024-05-25 23:59:59', '比赛将按规则进行评比', '参赛队伍需按照规定完成题目', '一等奖：5000元，二等奖：3000元，三等奖：1000元', '/image/01.png', 1, 'B组织者', 0, 0);
 INSERT INTO `matches` VALUES (2, '第一届 全国大学生数学建模竞赛', '中国大学生数学建模竞赛组委会', '张欢欢', '王珊珊', '孙莎莎', '2024-07-10 09:00:00', '2024-07-11 18:00:00', '2024-06-30 23:59:59', '比赛将在48小时内完成', '参赛队伍需根据题目要求进行数学建模', '一等奖：8000元，二等奖：5000元，三等奖：2000元', '/image/02.png', 2, 'B组织者', 0, 0);
 INSERT INTO `matches` VALUES (3, '第一届 全国大学生英语演讲比赛', '中国大学生英语演讲比赛组委会', '孙欣欣', '王丹丹', '赵琳琳', '2024-06-01 08:00:00', '2024-06-01 18:00:00', '2024-05-25 23:59:59', '比赛将按规则进行评比', '参赛队伍需按照规定完成题目', '一等奖：6000元，二等奖：3000元，三等奖：2000元', '/image/03.png', 3, 'B组织者', 0, 0);
 INSERT INTO `matches` VALUES (4, '第二届 全国大学生程序设计竞赛', '中国大学生程序设计竞赛组委会', '李娜娜', '刘丽丽', '林玲玲', '2025-06-01 08:00:00', '2025-06-01 18:00:00', '2025-05-25 23:59:59', '比赛将按规则进行评比', '参赛队伍需按照规定完成题目', '11一等奖：5000元，二等奖：3000元，三等奖：1000元', '/image/01.png', 1, 'B组织者', 0, 1);
-INSERT INTO `matches` VALUES (5, '第二届 全国大学生数学建模竞赛', '中国大学生数学建模竞赛组委会', '张欢欢', '王珊珊', '孙莎莎', '2025-07-10 09:00:00', '2025-07-11 18:00:00', '2025-06-30 23:59:59', '比赛将在48小时内完成', '参赛队伍需根据题目要求进行数学建模', '一等奖：8000元，二等奖：5000元，三等奖：2000元', '/image/02.png', 2, 'B组织者', 0, 0);
-INSERT INTO `matches` VALUES (6, '第二届 全国大学生英语演讲比赛', '中国大学生英语演讲比赛组委会', '孙欣欣', '王丹丹', '赵琳琳', '2025-06-01 08:00:00', '2025-06-01 18:00:00', '2025-05-25 23:59:59', '比赛将按规则进行评比', '参赛队伍需按照规定完成题目', '一等奖：6000元，二等奖：3000元，三等奖：2000元	', '/image/03.png', 3, 'B组织者', 0, 0);
+INSERT INTO `matches` VALUES (5, '第二届 全国大学生数学建模竞赛', '中国大学生数学建模竞赛组委会', '张欢欢', '王珊珊', '孙莎莎', '2025-07-10 09:00:00', '2025-07-11 18:00:00', '2025-06-30 23:59:59', '比赛将在48小时内完成', '参赛队伍需根据题目要求进行数学建模', '一等奖：8000元，二等奖：5000元，三等奖：2000元', '/image/02.png', 2, 'B组织者', 0, 1);
+INSERT INTO `matches` VALUES (6, '第二届 全国大学生英语演讲比赛', '中国大学生英语演讲比赛组委会', '孙欣欣', '王丹丹', '赵琳琳', '2025-06-01 08:00:00', '2025-06-01 18:00:00', '2025-05-25 23:59:59', '比赛将按规则进行评比', '参赛队伍需按照规定完成题目', '一等奖：6000元，二等奖：3000元，三等奖：2000元	', '/image/03.png', 3, 'B组织者', 0, 1);
 
 -- ----------------------------
 -- Table structure for participation_record
@@ -101,6 +142,81 @@ CREATE TABLE `participation_record`  (
 -- Records of participation_record
 -- ----------------------------
 INSERT INTO `participation_record` VALUES (1, 4);
+INSERT INTO `participation_record` VALUES (2, 4);
+INSERT INTO `participation_record` VALUES (2, 6);
+
+-- ----------------------------
+-- Table structure for problem
+-- ----------------------------
+DROP TABLE IF EXISTS `problem`;
+CREATE TABLE `problem`  (
+  `problem_id` int NOT NULL AUTO_INCREMENT,
+  `match_id` int NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`problem_id`) USING BTREE,
+  INDEX `match_id`(`match_id`) USING BTREE,
+  CONSTRAINT `problem_ibfk_1` FOREIGN KEY (`match_id`) REFERENCES `matches` (`match_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of problem
+-- ----------------------------
+INSERT INTO `problem` VALUES (1, 4, '两数之和', '给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。', 'ALGORITHM', '2025-03-01 10:00:00');
+INSERT INTO `problem` VALUES (2, 4, '字符串匹配', '实现一个简单的字符串匹配算法，找出字符串 S 中第一次出现字符串 T 的位置。', 'ALGORITHM', '2025-03-01 10:00:00');
+INSERT INTO `problem` VALUES (3, 5, '城市交通优化', '某城市交通网络拥堵严重，请建立数学模型优化交通信号灯配时方案，以减少拥堵。', 'MODELING', '2025-03-01 10:00:00');
+INSERT INTO `problem` VALUES (4, 5, '疫情传播预测', '建立数学模型预测新型疾病在人群中的传播趋势，并提出有效的防控措施。', 'MODELING', '2025-03-01 10:00:00');
+INSERT INTO `problem` VALUES (5, 6, '全球环境保护', '请就\"全球环境保护\"这一主题，准备一份3-5分钟的英语演讲。', 'SPEAKING', '2025-03-01 10:00:00');
+INSERT INTO `problem` VALUES (6, 6, '文化多样性与全球化', '探讨文化多样性在全球化背景下的重要性，以及如何保护和促进文化多样性。', 'SPEAKING', '2025-03-01 10:00:00');
+
+-- ----------------------------
+-- Table structure for result
+-- ----------------------------
+DROP TABLE IF EXISTS `result`;
+CREATE TABLE `result`  (
+  `result_id` int NOT NULL AUTO_INCREMENT,
+  `match_id` int NOT NULL,
+  `team_id` int NOT NULL,
+  `total_score` decimal(10, 2) NOT NULL DEFAULT 0.00,
+  `rank` int NULL DEFAULT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`result_id`) USING BTREE,
+  INDEX `match_id`(`match_id`) USING BTREE,
+  INDEX `team_id`(`team_id`) USING BTREE,
+  CONSTRAINT `result_ibfk_1` FOREIGN KEY (`match_id`) REFERENCES `matches` (`match_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `result_ibfk_2` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of result
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for submission
+-- ----------------------------
+DROP TABLE IF EXISTS `submission`;
+CREATE TABLE `submission`  (
+  `submission_id` int NOT NULL AUTO_INCREMENT,
+  `team_id` int NOT NULL,
+  `match_id` int NOT NULL,
+  `problem_id` int NOT NULL,
+  `content_url` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `submit_time` datetime NOT NULL,
+  `status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`submission_id`) USING BTREE,
+  INDEX `team_id`(`team_id`) USING BTREE,
+  INDEX `match_id`(`match_id`) USING BTREE,
+  INDEX `problem_id`(`problem_id`) USING BTREE,
+  CONSTRAINT `submission_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `submission_ibfk_2` FOREIGN KEY (`match_id`) REFERENCES `matches` (`match_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `submission_ibfk_3` FOREIGN KEY (`problem_id`) REFERENCES `problem` (`problem_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of submission
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for team
@@ -117,6 +233,7 @@ CREATE TABLE `team`  (
 -- Records of team
 -- ----------------------------
 INSERT INTO `team` VALUES (1, '教改一队', '/image/team/1.png');
+INSERT INTO `team` VALUES (2, '大角牛向前冲', '/image/team/1.png');
 
 -- ----------------------------
 -- Table structure for team_member
@@ -138,8 +255,11 @@ CREATE TABLE `team_member`  (
 -- ----------------------------
 -- Records of team_member
 -- ----------------------------
+INSERT INTO `team_member` VALUES ('S20150322', 2, '迟尚熙', '队员');
+INSERT INTO `team_member` VALUES ('S21008020411', 2, '迟宽鹏', '队长');
 INSERT INTO `team_member` VALUES ('S21008020412', 1, '王睿', '队员');
 INSERT INTO `team_member` VALUES ('S21008021019', 1, '刘洋', '队长');
+INSERT INTO `team_member` VALUES ('Tliutong', 2, '刘彤', '教师');
 INSERT INTO `team_member` VALUES ('Tshengyu', 1, '盛雨', '教师');
 
 -- ----------------------------
@@ -160,17 +280,18 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('21008020316', '白杨', '21008020316@neu.com', '@Baiyang123', '/image/profile/5.png');
 INSERT INTO `user` VALUES ('B组织者', '组织者', '组织者@admin.com', 'admin', '/image/profile/10.png');
 INSERT INTO `user` VALUES ('S20150322', '迟尚熙', '20150322@edu.cn', '@Shengyu123', '#');
 INSERT INTO `user` VALUES ('S21008020303', '李志明', '21008020303@neu.com', '@Lizhiming123', '/image/profile/8.png');
+INSERT INTO `user` VALUES ('S21008020316', '白杨', '21008020316@neu.com', '@Baiyang123', '/image/profile/5.png');
 INSERT INTO `user` VALUES ('S21008020320', '胡皓元', '21008020320@neu.com', '@Huhaoyuan123', '/image/profile/9.png');
-INSERT INTO `user` VALUES ('S21008020326', '韩佳樾', '21008020326@neu.com', '@Hanjiayue123', '/image/profile/7.png');
-INSERT INTO `user` VALUES ('S21008020411', '迟宽鹏', '21008020411@neu.com', '@Chikuanpeng123', '/image/profile/11.png');
+INSERT INTO `user` VALUES ('S21008020326S', '韩佳樾', '21008020326@neu.com', '@Hanjiayue123', '/image/profile/7.png');
+INSERT INTO `user` VALUES ('S21008020411', '迟宽鹏', '21008020411@neu.com', '@Chikuanpeng123', '/image/profile/14.png');
 INSERT INTO `user` VALUES ('S21008020412', '王睿', '21008020412@neu.com', '@Wangrui123', '/image/profile/6.png');
 INSERT INTO `user` VALUES ('S21008020425', '杨子骏', '21008020425@neu.com', '@Yangzijun123', '/image/profile/12.png');
 INSERT INTO `user` VALUES ('S21008021011', '杨皓东', '21008021011@neu.com', '@Haodong123', '/image/profile/13.png');
 INSERT INTO `user` VALUES ('S21008021019', '刘洋', '21008021019@edu.com', '@Liuyang123', '/image/profile/14.png');
+INSERT INTO `user` VALUES ('Tliutong', '刘彤', 'liutong@neu.com', '@liutong123', '#');
 INSERT INTO `user` VALUES ('Tshengyu', '盛雨', 'shengyu@neu.com', '@Shengyu123', '/image/profile/15.png');
 
 SET FOREIGN_KEY_CHECKS = 1;
